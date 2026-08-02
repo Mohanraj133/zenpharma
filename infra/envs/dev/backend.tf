b7497f4dbe32a4aa92b833a5bcce134ba66ba8e0
root <root@ip-172-31-8-72.ap-south-1.compute.internal>
2026-08-02 05:23:22 +0000

    Updated s3 bucket name

--- infra/envs/dev/backend.tf
+++ infra/envs/dev/backend.tf
@@ -1,9 +1,10 @@
 terraform {
   backend "s3" {
-    bucket       = "zen-pharma-terraform-state-ravdy"  # Replace with your S3 bucket name
+    bucket       = "devops-mohan-test-4419933"  # Replace with your S3 bucket name
     key          = "envs/dev/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
     use_lockfile = true   # S3 native locking (Terraform >= 1.11)
   }
 }
+ 
