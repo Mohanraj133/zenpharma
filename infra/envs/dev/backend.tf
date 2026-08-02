c87923711a6c1d98120f90fa6fb24e55f4ba242e
Mohanraj133 <107941522+Mohanraj133@users.noreply.github.com>
2026-08-02 11:01:34 +0530

    Update backend.tf

--- infra/envs/dev/backend.tf
+++ infra/envs/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "devops-mohan-test-4419933"  # Replace with your S3 bucket name
+    bucket       = "zen-pharma-terraform-state-devops-mohan-test-4419933"  # Replace with your S3 bucket name
     key          = "envs/dev/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
